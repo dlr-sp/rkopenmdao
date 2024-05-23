aa6eb94c91a4ec2d956b4cbe6307c549d3ba31d2
Florian Roß <florian.ross@dlr.de>
2024-05-23 13:52:43 +0200

    Added long description to setup.py.

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+from pathlib import Path
+
+this_dir = Path(__file__).parent
+long_description = (this_dir / "README.md").read_text()
 setup(
     name="rkopenmdao",
     version="0.1",
     description="Runge-Kutta time integration in OpenMDAO",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author="Deutsches Zentrum fuer Luft- und Raumfahrt e.V. (DLR)",
     author_email="florian.ross@dlr.de",
     packages=find_packages("src"),
