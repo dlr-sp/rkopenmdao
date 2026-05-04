47135f7665390f5f3a45a6f1f563f4ee6ca62714
shuv_im <imri.shuvi@dlr.de>
2026-05-04 12:11:39 +0200

    Fixes:
    test_error_measurer.py:
    instantiation of classes using factories to avoid shared mutable objects.

--- tests/test_error_measurer.py
+++ tests/test_error_measurer.py
@@ -15,40 +15,57 @@ from rkopenmdao.discretized_ode.discretized_ode import DiscretizedODEResultState
 from .odes import RootODE
 
 
+@pytest.mark.mpi
 @pytest.mark.parametrize(
-    "measurer, estimate, state, ode, expected_measure",
+    "measurer_factory, estimate, state, ode_factory, expected_measure",
     [
-        (SimpleErrorMeasurer(), np.ones(1), np.full(1, 2.0), RootODE(), 1.0),
-        (ImprovedErrorMeasurer(), np.ones(1), np.full(1, 2.0), RootODE(), 1 / 3),
         (
-            ImprovedErrorMeasurer(eta=1e-3),
+            SimpleErrorMeasurer,
             np.ones(1),
             np.full(1, 2.0),
-            RootODE(),
+            RootODE,
+            1.0,
+        ),
+        (
+            ImprovedErrorMeasurer,
+            np.ones(1),
+            np.full(1, 2.0),
+            RootODE,
+            1 / 3,
+        ),
+        (
+            lambda: ImprovedErrorMeasurer(eta=1e-3),
+            np.ones(1),
+            np.full(1, 2.0),
+            RootODE,
             1 / 1002,
         ),
         (
-            ImprovedErrorMeasurer(eps=1e-3),
+            lambda: ImprovedErrorMeasurer(eps=1e-3),
             np.ones(1),
             np.full(1, 2.0),
-            RootODE(),
+            RootODE,
             1000 / 2001,
         ),
     ],
 )
 def test_error_measurer(
-    measurer: ErrorMeasurer,
+    measurer_factory,
     estimate: np.ndarray,
     state: np.ndarray,
-    ode: DiscretizedODE,
+    ode_factory,
     expected_measure: float,
 ):
     """
     Tests the correct calculation of the error measure based on an estimate and ODE.
     """
+    measurer: ErrorMeasurer = measurer_factory()
+    ode: DiscretizedODE = ode_factory()
+
     result = measurer.get_measure(
         DiscretizedODEResultState(None, estimate, None),
         DiscretizedODEResultState(None, state, None),
         ode,
     )
+
     assert result == pytest.approx(expected_measure)
